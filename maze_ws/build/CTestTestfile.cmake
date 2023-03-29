c1390166760f222923b811242fe722ddb5bcede8
Bruno <bctat@cin.ufpe.br>
2023-03-29 11:17:33 -0300

    Creating workspace and package

---
+++ maze_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bctat/TIAGo-maze-2/maze_ws/src
+# Build directory: /home/bctat/TIAGo-maze-2/maze_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
