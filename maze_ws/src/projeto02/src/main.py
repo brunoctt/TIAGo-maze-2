de21e7f0e9ace1e8f15a0d311601bc96c0600e99
Bruno <bctat@cin.ufpe.br>
2023-03-31 16:53:34 -0300

    Robot turning head at intersection

--- maze_ws/src/projeto02/src/main.py
+++ maze_ws/src/projeto02/src/main.py
@@ -6,11 +6,12 @@ from math import ceil, pi, asin
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from tf.transformations import euler_from_quaternion
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 class myRobot():
 
-    distance = 1.2
+    distance = 1
 
     def __init__(self):
         print('init')
@@ -21,16 +22,15 @@ class myRobot():
         # Client Service camera
         self.base_pub = rospy.Publisher('/nav_vel', Twist, queue_size=1)
         self.orientation = None
-        # Publisher cabeca
+        self.pub_head = rospy.Publisher('/head_controller/command', JointTrajectory, queue_size=1)
+        # Executando laço 4 vezes por segundo
+        self.rate = rospy.Rate(4)
 
     def callback_odometria(self, msg: Odometry):
         # print('callback odometria')
         # Armazenar os dados de odometria
         q = msg.pose.pose.orientation
         self.orientation = round(euler_from_quaternion([q.x, q.y, q.z, q.w])[-1], 2)
-        # print(self.orientation)
-        # pass
-        
 
     def callback_laser(self, msg: LaserScan):
         # print('callback laser')
@@ -43,7 +43,9 @@ class myRobot():
         print('move straight')
         move = Twist()
         dist_min_central = min(self.laser_ranges[222:-222])
-        move.linear.x = 5 * (dist_min_central - 1)
+        k = 2
+        dist = 1
+        move.linear.x = k * (dist_min_central - dist)
         self.base_pub.publish(move)
         
     def stop(self):
@@ -58,12 +60,10 @@ class myRobot():
         initial_ori = self.orientation
         move = Twist()
         diff = min(abs(self.orientation - initial_ori), (self.orientation - initial_ori)%pi)
-        print(diff)
         while diff < pi/2 - 0.02:
             move.angular.z = (abs(angle) - diff) * (angle//abs(angle))
             self.base_pub.publish(move)
             diff = min(abs(self.orientation - initial_ori), (self.orientation - initial_ori)%pi)
-            print(diff)
         
     def turn_right(self):
         self.turn(-pi/2)
@@ -81,10 +81,11 @@ class myRobot():
         right = min(self.laser_ranges[:side])
         straight = min(self.laser_ranges[side:-side])
         left = min(self.laser_ranges[-side:])
-        # print('-'*30)
-        # print(left)
-        # print(right)
-        if (right < self.distance and left < self.distance) or straight > self.distance:
+        print('-'*30)
+        print(left)
+        print(straight)
+        print(right)
+        if (right < self.distance and left < self.distance) or straight > 1.2:
             return 2  # Andar pra frente
         elif right >= self.distance and left >= self.distance:
             return 1  # Tirar fotos
@@ -94,17 +95,54 @@ class myRobot():
             return 4  # Virar para direita
         else:
             return 0
+         
+    def image_processing(self):
+        # Virar a cabeca
+        self.move_head()
+        
+    def move_head(self):
+        cmd = JointTrajectory()
+        cmd.joint_names.append("head_1_joint")
+        cmd.joint_names.append("head_2_joint")
+
+        point = JointTrajectoryPoint()
+        point.positions = [0] * 2
+        point.velocities = [0] * 2
+        point.accelerations = [0] * 2
+        point.time_from_start = rospy.Duration(1)
+
+        cmd.points.append(point)
+        
+        self.rot_head_left(cmd)
+        self.rot_head_right(cmd)
+
+    def rot_head_left(self, cmd):
+        angle = 0.0
+        while angle < 1.6:
+            cmd.points[0].positions[0] = angle
+            cmd.points[0].time_from_start = rospy.Duration(1)
+
+            self.pub_head.publish(cmd)
+            angle += 0.2
+            self.rate.sleep()
             
+    def rot_head_right(self, cmd):
+        angle = 0.0
+        while angle > -1.6:
+            cmd.points[0].positions[0] = angle
+            cmd.points[0].time_from_start = rospy.Duration(1)
+
+            self.pub_head.publish(cmd)
+            angle -= 0.2
+            self.rate.sleep()
 
 if __name__ == '__main__':
 
     rospy.init_node('maze_runner')
 
     tiago = myRobot()
+    sleep(5)
     
-    # Executando laço 4 vezes por segundo
-    rate = rospy.Rate(4)
-
     state = 0
     while not rospy.is_shutdown():
         if state == 0:
@@ -128,4 +166,4 @@ if __name__ == '__main__':
         # tiago.turn(pi/2)
         # break
         print(state)
-        rate.sleep()
+        tiago.rate.sleep()
